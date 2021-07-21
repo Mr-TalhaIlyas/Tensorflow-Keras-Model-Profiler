9464f7eb6f3b419de4812bcbe30205609a55832d
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2021-07-21 16:09:12 +0900

    Update utils.py

--- model_profiler/utils.py
+++ model_profiler/utils.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 from tensorflow.python.client import device_lib
 
 
+
+
 #%%
 '''
 For appropriate units
@@ -16,10 +18,10 @@ multiplier = {
     'Million': 1 / 10**6,  # paprmeter count unit millions
     'Billion': 1 / 10**9,  # paprmeter count unit billions
 }
-
+# will make the ouput in appropriate unit
 def multiply(fn):
     def deco(units, *args, **kwds):
-        return multiplier.get(units, -1) * fn(*args, **kwds)
+        return np.round(multiplier.get(units, -1) * fn(*args, **kwds), 4)
     return deco
 #%%
 def get_available_gpus():
@@ -30,6 +32,8 @@ def get_available_gpus():
         names = []
         for i in gpus:
             names.append(i[-1:])
+        if len(gpus) == 0:
+            names = 'None Detected'
     except:
         names = 'None Detected'
     return names
@@ -37,13 +41,23 @@ def get_available_gpus():
 @multiply
 def get_param(model):
     '''Return Model Parameters in unit scale'''
-    trainable_param = model.count_params()
-    return trainable_param    #(trainable_param / 1000**2)
+    if isinstance(model, list):
+        trainable_param = 0
+        for l in model:
+            trainable_param += l.count_params()
+    else:
+        trainable_param = model.count_params()
+    return trainable_param   
 #%%
 @multiply
 def mem_for_storing_weights(model):
     '''Return mnemory required to store model parameters in bytes'''
-    mem_req = model.count_params() * 4
+    if isinstance(model, list):
+        mem_req = 0
+        for l in model:
+            mem_req += l.count_params() * 4
+    else:
+        mem_req = model.count_params() * 4
     return mem_req
 #%%
 @multiply
@@ -66,27 +80,47 @@ def keras_model_memory_usage(model, batch_size):
     default_dtype = tf.keras.backend.floatx()
     shapes_mem_count = 0
     internal_model_mem_count = 0
-    for layer in model.layers:
-        if isinstance(layer, tf.keras.Model):
-            internal_model_mem_count += keras_model_memory_usage(
-                layer, batch_size=batch_size
-            )
-        single_layer_mem = tf.as_dtype(layer.dtype or default_dtype).size
-        out_shape = layer.output_shape
-        if isinstance(out_shape, list):
-            out_shape = out_shape[0]
-        for s in out_shape:
-            if s is None:
-                continue
-            single_layer_mem *= s
-        shapes_mem_count += single_layer_mem
-
-    trainable_count = sum(
-        [tf.keras.backend.count_params(p) for p in model.trainable_weights]
-    )
-    non_trainable_count = sum(
-        [tf.keras.backend.count_params(p) for p in model.non_trainable_weights]
-    )
+    
+    if isinstance(model, list):
+        trainable_count = 0
+        non_trainable_count = 0
+        for layer in model:
+            
+            single_layer_mem = tf.as_dtype(layer.dtype or default_dtype).size
+            out_shape = layer.output_shape
+            if isinstance(out_shape, list):
+                out_shape = out_shape[0]
+            for s in out_shape:
+                if s is None:
+                    continue
+                single_layer_mem *= s
+            shapes_mem_count += single_layer_mem
+            
+            trainable_count += sum(
+                            [tf.keras.backend.count_params(p) for p in layer.trainable_weights]
+                            )
+            non_trainable_count += sum(
+                            [tf.keras.backend.count_params(p) for p in layer.non_trainable_weights]
+                            )
+    else:
+        for layer in model.layers:
+            
+            single_layer_mem = tf.as_dtype(layer.dtype or default_dtype).size
+            out_shape = layer.output_shape
+            if isinstance(out_shape, list):
+                out_shape = out_shape[0]
+            for s in out_shape:
+                if s is None:
+                    continue
+                single_layer_mem *= s
+            shapes_mem_count += single_layer_mem
+    
+        trainable_count = sum(
+            [tf.keras.backend.count_params(p) for p in model.trainable_weights]
+        )
+        non_trainable_count = sum(
+            [tf.keras.backend.count_params(p) for p in model.non_trainable_weights]
+        )
 
     total_memory = (
         batch_size * shapes_mem_count
@@ -95,38 +129,50 @@ def keras_model_memory_usage(model, batch_size):
         + non_trainable_count
     )
     
-    # convery into bytes to GB
-    # total_memory = total_memory / 1024**3
+    
     return total_memory       
 #%%
 # bunch of cal per layer
 def count_linear(layers):
     MAC = layers.output_shape[1] * layers.input_shape[1]
-    if layers.get_config()["use_bias"]:
-        ADD = layers.output_shape[1]
-    else:
+    try:
+        if layers.get_config()["use_bias"]:
+            ADD = layers.output_shape[1]
+        else:
+            ADD = 0
+    except KeyError:
         ADD = 0
     return MAC*2 + ADD
 
 def count_conv2d(layers, log = False):
-    # if log:
-    #     print(layers.get_config())
-    # number of conv operations = input_h * input_w / stride = output^2
-    numshifts = int(layers.output_shape[1] * layers.output_shape[2])
+    
+    if layers.output_shape[1] != None:
+        numshifts = int(layers.output_shape[1] * layers.output_shape[2])
+    elif layers.output_shape[1] == None:
+        numshifts = int(layers.output_shape[-1])
     
     # MAC/convfilter = kernelsize^2 * InputChannels * OutputChannels
-    MACperConv = layers.get_config()["kernel_size"][0] * layers.get_config()["kernel_size"][1] * layers.input_shape[3] * layers.output_shape[3]
+    try:
+        MACperConv = layers.get_config()["kernel_size"][0] * layers.get_config()["kernel_size"][1] * layers.input_shape[3] * layers.output_shape[3]
+    except KeyError:
+        MACperConv = 0
+        pass
     
-    if layers.get_config()["use_bias"]:
-        ADD = layers.output_shape[3]
-    else:
+    try:
+        if layers.get_config()["use_bias"]:
+            ADD = layers.output_shape[3]
+        else:
+            ADD = 0
+    except KeyError:
         ADD = 0
+        pass
         
     return MACperConv * numshifts * 2 + ADD
+
 @multiply
 def count_flops(model, log = False):
     '''
-    Parameters
+    ParametersNo documen
     ----------
     model : A keras or TF model
     Returns
@@ -136,14 +182,33 @@ def count_flops(model, log = False):
     '''
     layer_flops = []
     # run through models
-    for layer in model.layers:
-        if "dense" in layer.get_config()["name"] or "fc" in layer.get_config()["name"]:
-            layer_flops.append(count_linear(layer))
-        elif "conv" in layer.get_config()["name"] and "pad" not in layer.get_config()["name"] and "bn" not in layer.get_config()["name"] and "relu" not in layer.get_config()["name"] and "concat" not in layer.get_config()["name"]:
-            layer_flops.append(count_conv2d(layer,log))
-        elif "res" in layer.get_config()["name"] and "branch" in layer.get_config()["name"]:
-            layer_flops.append(count_conv2d(layer,log))
-        elif "stage" in layer.get_config()['name']:
-            layer_flops.append(count_conv2d(layer,log))
-            
-    return np.sum(layer_flops)     #/10**11
+    if isinstance(model, list):
+        for layer in model:
+            if "dense" in layer.get_config()["name"] or "fc" in layer.get_config()["name"] or "squeeze" in layer.get_config()["name"]:
+                layer_flops.append(count_linear(layer))
+            elif "conv" in layer.get_config()["name"] :
+                layer_flops.append(count_conv2d(layer,log))
+            elif "dwconv" in layer.get_config()["name"]:
+                layer_flops.append(count_conv2d(layer,log))
+            elif "expand" in layer.get_config()["name"]:
+                layer_flops.append(count_conv2d(layer,log))
+            elif "res" in layer.get_config()["name"]:
+                layer_flops.append(count_conv2d(layer,log))
+            elif "stage" in layer.get_config()['name']:
+                layer_flops.append(count_conv2d(layer,log))
+    else:    
+        for layer in model.layers:
+            if "dense" in layer.get_config()["name"] or "fc" in layer.get_config()["name"] or "squeeze" in layer.get_config()["name"]:
+                layer_flops.append(count_linear(layer))
+            elif "conv" in layer.get_config()["name"]:
+                layer_flops.append(count_conv2d(layer,log))
+            elif "dwconv" in layer.get_config()["name"]:
+                layer_flops.append(count_conv2d(layer,log))
+            elif "expand" in layer.get_config()["name"]:
+                layer_flops.append(count_conv2d(layer,log))
+            elif "res" in layer.get_config()["name"]:
+                layer_flops.append(count_conv2d(layer,log))
+            elif "stage" in layer.get_config()['name']:
+                layer_flops.append(count_conv2d(layer,log))
+    
+    return np.sum(layer_flops, dtype=np.int64, initial=0)
