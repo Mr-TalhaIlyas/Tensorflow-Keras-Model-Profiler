47126f2b4c7ed20f09e02f5f6e1ca7d607808b8a
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2021-07-21 16:08:55 +0900

    Update profiler.py

--- model_profiler/profiler.py
+++ model_profiler/profiler.py
@@ -5,59 +5,82 @@ Created on Tue Apr  6 17:45:33 2021
 @author: Talha
 """
 
+#from model_profiler.utils import get_available_gpus, get_param, keras_model_memory_usage, count_flops, mem_for_storing_weights
 from utils import get_available_gpus, get_param, keras_model_memory_usage, count_flops, mem_for_storing_weights
-#%
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
+from tensorflow.python.keras.engine.functional import Functional
 import numpy as np
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.applications import EfficientNetB4, EfficientNetB7
 from tabulate import tabulate
 
+
 Batch_size = 1
-model = EfficientNetB4(
-    include_top=True,
-    weights=None,
-    input_tensor=None,
-    input_shape=None,
-    pooling=None,
-    classes=1000,
-    classifier_activation="softmax")
-
-#model.save_weights('ef4.h5')
-# model.fit(data, one_hot_labels, epochs=Epoch, batch_size=Batch_size,
-#         verbose=1)
-#%%
-use_units = ['GPU IDs', 'BFLOPs', 'GB', 'Million', 'MB']
-
-
-
-def model_profiler(model, Batch_size, use_units):
-    
-    gpus = get_available_gpus()
-    flops = count_flops(use_units[1], model, Batch_size)
-    mem = keras_model_memory_usage(use_units[2], model, Batch_size)
-    param = get_param(use_units[3], model)
-    mem_req = mem_for_storing_weights(use_units[4], model)
-    
-    values = [gpus, flops, mem, param, mem_req]
-    return values
-
-values = model_profiler(model, Batch_size, use_units)
+units = ['GPU IDs', 'BFLOPs', 'GB', 'Million', 'MB']
 
 Profile = ['Selected GPUs', 'No. of FLOPs', 'GPU Memory Requirement',
            'Model Parameters', 'Memory Required by Model Weights']
 
-full_profile = np.concatenate((
+
+def model_profiler(model, Batch_size, profile=Profile, use_units=units, verbose=0):
+    '''
+    Parameters
+    ----------
+    model : a keras/tensorflow compiled or uncompiled model
+    Batch_size : an int default to 1.
+    Profile : a list of profile characterstics
+    use_units : units for those characterstics
+    verbose: whether to print out the model profile or not [verbose > 0] will 
+            print out the profile
+    Returns
+    -------
+    profile: a ordered pretty string table containing model profile 
+            alos prints out the model profile
+
+    '''
+    gpus = get_available_gpus()
+    ###
+    # if you have used a keras/tf built in model than that model will be an object of
+    # tensorflow/keras engine functional, meaning one functional layer will be packing
+    # all the layers. So, we first need to look inside the functional layer
+    ###
+    
+    func_idx = 0
+    found_func = False
+    for j, layer in enumerate(model.layers):
+        if isinstance(layer, Functional):
+            func_idx = j
+            found_func = True
+    
+    if found_func:
+        flops = count_flops(use_units[1], model.layers[func_idx], Batch_size)
+        mem = keras_model_memory_usage(use_units[2], model.layers[func_idx], Batch_size)
+        param = get_param(use_units[3], model.layers[func_idx])
+        mem_req = mem_for_storing_weights(use_units[4], model.layers[func_idx])
+
+        flops += count_flops(use_units[1], model.layers[func_idx+1:], Batch_size)
+        mem += keras_model_memory_usage(use_units[2], model.layers[func_idx+1:], Batch_size)
+        param += get_param(use_units[3], model.layers[func_idx+1:])
+        mem_req += mem_for_storing_weights(use_units[4], model.layers[func_idx+1:])
+    else:
+        flops = count_flops(use_units[1], model, Batch_size)
+        mem = keras_model_memory_usage(use_units[2], model, Batch_size)
+        param = get_param(use_units[3], model)
+        mem_req = mem_for_storing_weights(use_units[4], model)
+    
+    values = [gpus, flops, mem, param, mem_req]
+    
+    full_profile = np.concatenate((
                                 np.asarray(Profile).reshape(-1,1),
                                 np.asarray(values).reshape(-1,1),
                                 np.asarray(use_units).reshape(-1,1)
                                 )
                             , 1)
-print(tabulate(
-                np.ndarray.tolist(full_profile),
-                headers = ["Model Profile", "Value", "Unit"],
-                tablefmt="github"
-                )
-    )
+    profile = tabulate(
+                    np.ndarray.tolist(full_profile),
+                    headers = ["Model Profile", "Value", "Unit"],
+                    tablefmt="github"
+                    )
+    if verbose > 0:
+        print(profile)
+    
+    return profile
+
+
